d19890b61ee9442a78a0132981d23fe3fa887b1e
salesluis <salesluis434@gmail.com>
2025-09-17 20:09:12 -0300

    feat: add Categorias and Produtos controllers with CRUD operations

---
+++ APICatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,76 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Filters;
+using APICatalogo.Models;
+using APICatalogo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CategoriasController : ControllerBase
+{
+    private readonly ICategoriaRepository _categoriaRepository;
+    private readonly ILogger<CategoriasController> _logger;
+
+    public CategoriasController(ICategoriaRepository categoriaRepository, ILogger<CategoriasController> logger)
+    {
+        _categoriaRepository = categoriaRepository;
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Categoria>> Get()
+    {
+        var categorias = _categoriaRepository.GetAllAsync();
+        return Ok(categorias);
+    }
+
+    [HttpGet("{id:int}", Name = "ObterCategoria")]
+    public ActionResult<Categoria> Get(int id)
+    {
+        var categoria = _categoriaRepository.GetById(id);
+
+        if (categoria is null)
+        {
+            _logger.LogWarning($"Categoria com id= {id} não encontrada...");
+            return BadRequest($"Categoria com id= {id} não encontrada...");
+        }
+        return Ok(categoria);
+    }
+
+    [HttpPost]
+    public ActionResult Post(Categoria categoria)
+    {
+        if (categoria is null)
+        {
+            _logger.LogWarning($"Dados inválidos...");
+            return BadRequest("Dados inválidos");
+        }
+
+        _categoriaRepository.Create(categoria);
+
+        return new CreatedAtRouteResult("ObterCategoria", new { id = categoria.CategoriaId }, categoria);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Categoria categoria)
+    {
+        if (id != categoria.CategoriaId)
+        {
+            _logger.LogWarning($"Dados inválidos...");
+            return BadRequest("Dados inválidos");
+        }
+
+        _categoriaRepository.Update(categoria);
+        return Ok(categoria);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var categoria = _categoriaRepository.Delete(id);
+        return Ok(categoria);
+    }
+}
